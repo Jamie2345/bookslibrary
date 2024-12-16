e14c0789637d91258209785904aeb080988eaf1c
Jamie Cliffe <jamiecliffe2@gmail.com>
2024-12-16 20:10:08 +0000

    moved the genre grouping and random results to the client side because the server will be a rasberry pi and I want to reduce the load on server

--- server/server.js
+++ server/server.js
@@ -23,46 +23,9 @@ app.get("/api/books", async (req, res) => {
 
     // Find matching documents
     const books = await booksCollection.find().toArray();
-    const randomBooks = pickRandomItems(books, 8);
-
-    const business_books = await booksCollection
-      .find({ "Book Genre": { $in: ["Business"] } })
-      .toArray();
-
-    const randomBusinessBooks = pickRandomItems(business_books, 4);
-
-    const philosophy_books = await booksCollection
-      .find({ "Book Genre": { $in: ["Philosophy"] } })
-      .toArray();
-    const randomPhilosophyBooks = pickRandomItems(philosophy_books, 4);
-
-    const personal_development_books = await booksCollection
-      .find({ "Book Genre": { $in: ["Personal Development"] } })
-      .toArray();
-    const randomPersonalDevelopmentBooks = pickRandomItems(
-      personal_development_books,
-      4
-    );
-
-    const history_books = await booksCollection
-      .find({ "Book Genre": { $in: ["History"] } })
-      .toArray();
-    const randomHistoryBooks = pickRandomItems(history_books, 4);
-
-    const fiction_books = await booksCollection
-      .find({ "Book Genre": { $in: ["Fiction"] } })
-      .toArray();
-    const randomFictionBooks = pickRandomItems(fiction_books, 4);
-
-    // Send the results as JSON
+    
     res.json({
       all_books: books,
-      random_books: randomBooks,
-      business_books: randomBusinessBooks,
-      philosophy_books: randomPhilosophyBooks,
-      personal_development_books: randomPersonalDevelopmentBooks,
-      history_books: randomHistoryBooks,
-      fiction_books: randomFictionBooks,
     });
   } catch (error) {
     console.error(error);
